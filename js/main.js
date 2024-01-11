7b18a046ea1b95f9239234932b568176ce39cb80
hkgonebad <harry007007@gmail.com>
2024-01-11 15:57:53 +0530

    Initial commit

--- js/main.js
+++ js/main.js
@@ -33,4 +33,39 @@ $(function() {
         });
     }
 
+    // Store Locator
+    const jlocator = $('#jlocator');
+    if (jlocator.length > 0) { 
+		jlocator.jlocator({
+			openInfoWindowOnStoreClick: true,
+            startZoom: 4, //initial map zoom (on page load)
+            storeZoom: 17, //map zoom on store click
+            latitude: 20.593684, //initial map latitude (on page load)
+            longitude: 78.962880, //initial map longitude (on page load)         
+            geolocation: true
+		});
+	}
+
+	var showData = $('.stores');
+
+    $.getJSON('json/data.json', function (data) {
+
+	    var items = data.markers.map(function (item) {
+		    return `
+            <div data-type="store" class="store box" data-latitude="${item.Lat}" data-longitude="${item.Long}">
+              <div class="art">
+                <h4 class="title" data-type="title">${item.Location}</h4>
+                <p data-type="address">${item.Address}</p>
+                <p data-type="city">${item.City}</p>
+                <p data-type="state">${item.State}</p>
+                <p data-type="zipcode">${item.Pincode}</p>
+                <p data-type="phone">${item.PhoneNumber}</p>
+              </div>
+            </div>`;
+		});
+
+	    showData.html(items);
+    });
+
+    showData.text('Loading the JSON file.');
 });
